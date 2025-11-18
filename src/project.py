bb6909ff6b4338e2869c2d30ff60488f3610dd79
Kristen Fazzini <krf230000@utdallas.edu>
2025-11-18 00:02:49 -0600

    added pygame.quit

--- src/project.py
+++ src/project.py
@@ -5,6 +5,7 @@ def main():
     pygame.display.set_caption("Nighttime Fun")
     resolution = (2048, 2048)
     screen = pygame.display.set_mode(resolution)
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
