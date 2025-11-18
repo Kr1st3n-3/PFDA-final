0e039e311106015b8ee8d71f97f97c4f74e0b967
Kristen Fazzini <krf230000@utdallas.edu>
2025-11-18 00:01:31 -0600

    added the main definition with the display name and size

--- src/project.py
+++ src/project.py
@@ -1,2 +1,10 @@
 import pygame, sys
 
+def main():
+    pygame.init()
+    pygame.display.set_caption("Nighttime Fun")
+    resolution = (2048, 2048)
+    screen = pygame.display.set_mode(resolution)
+
+if __name__ == "__main__":
+    main()
