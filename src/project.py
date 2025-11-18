4766b0f4faab9efe3c4aa976920fdc75367c80b3
Kristen Fazzini <krf230000@utdallas.edu>
2025-11-18 00:09:52 -0600

    closed my event loop with pygame.quit in a if statement

--- src/project.py
+++ src/project.py
@@ -5,6 +5,12 @@ def main():
     pygame.display.set_caption("Nighttime Fun")
     resolution = (2048, 2048)
     screen = pygame.display.set_mode(resolution)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("final BG.PNG")
     pygame.quit()
 
 if __name__ == "__main__":
