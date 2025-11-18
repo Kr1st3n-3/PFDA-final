470bf5df43ae589523e2f2895e86333ad50534d8
Kristen Fazzini <krf230000@utdallas.edu>
2025-11-18 00:58:15 -0600

    imported my bg image into my code. Resized it cause the og pixel size was way to big. Then  added a screen.blit and .flip to actually get it to show up.

--- src/project.py
+++ src/project.py
@@ -1,16 +1,21 @@
 import pygame, sys
+from PIL import Image
 
 def main():
     pygame.init()
     pygame.display.set_caption("Nighttime Fun")
-    resolution = (2048, 2048)
+    resolution = (800, 800)
     screen = pygame.display.set_mode(resolution)
+    background_img = pygame.image.load("final_BG.PNG").convert_alpha()
+    background_img = pygame.transform.scale(background_img, (resolution))
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill("final BG.PNG")
+        screen.blit(background_img, (0,0))
+        pygame.display.flip()
+                
     pygame.quit()
 
 if __name__ == "__main__":
